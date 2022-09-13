806a4c7c81b3df1485a38e6f4884f4a0118f1b8f
abdulshakir47 <112155594+abdulshakir47@users.noreply.github.com>
2022-09-13 10:24:45 +0530

    Add files via upload

---
+++ global.js
@@ -0,0 +1,11 @@
+console.log(global);
+
+setTimeout(()=>{
+    console.log("Hii this is shakir");
+    clearInterval(s)
+},3000)
+
+var s = setInterval(()=>{
+    console.log("Hi this will print every 3 seconds");
+},1000)
+
